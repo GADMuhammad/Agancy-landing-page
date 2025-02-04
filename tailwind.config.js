6a0db64357caef09c5064ab6970534d25d3a5073
Muhammad Gad <106442148+GADMuhammad@users.noreply.github.com>
2025-02-04 16:45:28 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,8 +44,8 @@ export default {
       },
 
       backgroundImage: {
-        headerBg: "url('public/desktop/image-header.jpg')",
-        headerMobileBg: "url('public/mobile/image-header.jpg')",
+        headerBg: "url('desktop/image-header.jpg')",
+        headerMobileBg: "url('mobile/image-header.jpg')",
       },
 
       boxShadow: {
